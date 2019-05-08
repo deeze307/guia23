8bdb0610869bd0ba65948ecf95da4de359af9209
Diego Maidana <deeze.designs@gmail.com>
2019-05-08 01:25:01 -0300

    Fix Home
    - Se arregla lista de publicidades más visitadas.
    - Se arregla lista de Nuevas publicidades (por provincia).
    - Se arregla Login nativo.

--- app/model/AdvertsingsCounter.php
+++ app/model/AdvertsingsCounter.php
@@ -75,21 +75,76 @@ class AdvertsingsCounter
     public function mostVisited()
     {
         try {
+            // Primero listo todas las publicaciones con mayor cantidad de visitas
+            // Logger::write("debug","[".date('d-m-Y h:i:s')."] Contador de visitas");
+            $this->db->join("advertsings a", "aco.advertsings_id = a.advertsing_id", "LEFT");
             $this->db->join("advertsing_detail ad", "a.advertsing_detail_id = ad.advertsing_detail_id", "LEFT");
-            $this->db->join("advertsings_categories ac", "ad.category_id = ac.advertsings_categories_id", "LEFT");
-            $this->db->join("advertsings_counter aco", "a.advertsing_id = aco.advertsings_id", "LEFT");
-            $this->db->join("cities c", "ad.city_id = c.city_id", "LEFT");
-            $this->db->join("provinces p", "ad.province_id = p.province_id", "LEFT");
-            $valuations = $this->db->subQuery();
-            $valuations->where('advertsing_id','a.advertsing_id');
-            $valuations->getOne('valuations','quantity');
-            $this->db->where('a.enabled', 'T')
-                    ->where('aco.counter', 'null', '!=')
-                    ->where('ad.city_id',$_SESSION['selected_city_id']);
             $this->db->orderBy('aco.counter','DESC');
-            $most_visited = $this->db->objectBuilder()->get('advertsings a', 9, 'a.advertsing_id,a.enabled,ac.name as category_name,c.name as city_name,p.name as province_name,ad.*,aco.counter,(SELECT IFNULL( ROUND(AVG(quantity)),0) from valuations where advertsing_id = a.advertsing_id limit 1) as valoraciones');
+            $adv_counter = $this->db->objectBuilder()->get('advertsings_counter aco',50,'aco.*,a.*,ad.*');
+            
+            $most_visited = array();
+            foreach($adv_counter as $adv)
+            {
+                $visit = [];
+                // Busco información de las publicaciones de los comercios
+                if($adv->category_id == NULL)
+                {
+                    // $this->db->join("advertsings_")
+                    $visit = $this->db->objectBuilder()->rawQueryOne(
+                        "select a.advertsing_id,
+                                a.enabled,
+                                ac.name as category_name,
+                                c.name as city_name,
+                                p.name as province_name,
+                                ad.title,
+                                ad.subtitle,
+                                ad.description,
+                                ad.commercial_image,
+                                aco.counter,
+                                (SELECT IFNULL( ROUND(AVG(quantity)),0) from valuations where advertsing_id = a.advertsing_id limit 1) as valoraciones
+                        from advertsings a
+                        left join advertsings_counter aco on a.advertsing_id = aco.advertsings_id
+                        left join advertsing_detail ad on a.advertsing_detail_id = ad.advertsing_detail_id
+                        left join advertsing_commerce acom on ad.commerce_id = acom.id
+                        left join advertsing_commerce_detail acomdet on acom.advertsing_commerce_detail_id = acomdet.advertsing_commerce_detail_id
+                        left join advertsings_categories ac on acomdet.category_id = ac.advertsings_categories_id
+                        left join cities c on acomdet.city_id = c.city_id
+                        left join provinces p on acomdet.province_id = p.province_id
+                        where a.advertsing_id  = ".$adv->advertsing_id."
+                        and a.enabled = 'T'
+                        and acomdet.city_id = ".$_SESSION['selected_city_id']."
+                        and acomdet.province_id = ".$_SESSION['selected_province_id']."
+                        limit 1"
+                    );
+                    
+                }
+                // Si no son comercios, son publicaciones de la página
+                else
+                {
+                    $visit = $this->db->objectBuilder()->rawQueryOne(
+                        "select a.advertsing_id,a.enabled,ac.name as category_name,c.name as city_name,p.name as province_name,ad.*,aco.counter,(SELECT IFNULL( ROUND(AVG(quantity)),0) from valuations where advertsing_id = a.advertsing_id limit 1) as valoraciones
+                        from advertsings a
+                        left join advertsings_counter aco on a.advertsing_id = aco.advertsings_id
+                        left join advertsing_detail ad on a.advertsing_detail_id = ad.advertsing_detail_id
+                        left join advertsings_categories ac on ad.category_id = ac.advertsings_categories_id
+                        left join cities c on ad.city_id = c.city_id
+                        left join provinces p on ad.province_id = p.province_id
+                        where a.advertsing_id  = ".$adv->advertsing_id."
+                        and a.enabled = 'T'
+                        and ad.city_id = ".$_SESSION['selected_city_id']."
+                        and ad.province_id = ".$_SESSION['selected_province_id']."
+                        limit 1"
+                    );
+                }
+                if($visit && (count($most_visited)) < 9)
+                {
+                    array_push($most_visited,$visit);
+                }
+            }
             return $most_visited;
+            
         } catch (Exception $ex){
+            Logger::write("error","[".date('d-m-Y h:i:s')."] ".$ex->getMessage());
             return $ex->getMessage();
         }
     }
